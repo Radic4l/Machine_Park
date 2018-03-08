60b6065893541b2abd240c71bf88a210228e16af
Radic4l <axel.dujardin@Digital-Grenoble.local>
2018-03-08 17:14:49 +0100

    created my first componement

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import test from './test.vue'
 new Vue({
   el: '#app',
   render: h => h(App)
---
+++ src/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="test" v-for="test in tests">
+    {{name}} {{status}}
+  </div>
+</template>
+<script>
+export default {
+  name: "test",
+  data: () => [
+    {name: "jean", status: "Alive"},
+    {name: "Marie", status: "Dead"},
+    {name: "charle", status: "Dead"},
+    {name: "Armond", status: "Alive"},
+    {name: "Guillaume", status: "Dead"},
+    {name: "Patick", status: "Alive"}
+  ]
+}
+</script>
+<style>
+#data {
+  color: red;
+}
+</style>
